0a7bf1d8053320b31901aaa05747437b06fa3b9e
Enes Sinan Parildi <enessinan.parildi@mail.utoronto.ca>
2018-10-20 03:20:08 -0400

    Update run_main.py

--- run_main.py
+++ run_main.py
@@ -4,7 +4,12 @@ import knn_regression_tensorflow
 
 if __name__ == "__main__":
     kset  = np.array([1, 3, 5, 10, 50])
+    lambdaset= np.linspace(0.1, 50 , 100)
+
     traindata, validdata, testdata  = knn_regression_tensorflow.make_dataset()
-    testloss, testpred, kval = knn_regression_tensorflow.main_graph(traindata, validdata, testdata, kset)
-    print('Test mse loss= ' + str(testloss))
-    knn_regression_tensorflow.plot_results(testdata, testpred, kval)
+    testlosshard, testpredhard, testlosssoft, testpredsoft, chosenk , chosenl = knn_regression_tensorflow.main_graph(traindata, validdata, testdata, kset, lambdaset)
+    print('Test mse loss hard = ' + str(testlosshard))
+    print('Test mse loss soft = ' + str(testlosssoft))
+
+    knn_regression_tensorflow.plot_results(testdata, testpredhard, chosenk)
+    knn_regression_tensorflow.plot_results(testdata, testpredsoft, chosenl)
